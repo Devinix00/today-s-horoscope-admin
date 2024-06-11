85a045f8b35988e174024316860e98c88b7399c9
Devinix00 <dpldpl123456@naver.com>
2024-06-11 12:35:53 +0900

    feat: 파이어 베이스 링크 등록

--- app/(withSidebar)/statistics/page.tsx
+++ app/(withSidebar)/statistics/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import MainContainer from '../../_components/mainContainer/MainContainer';
-import ContentTitle from '../../_components/contentTitle/ContentTitle';
-import Link from 'next/link';
+import React from "react";
+import MainContainer from "../../_components/mainContainer/MainContainer";
+import ContentTitle from "../../_components/contentTitle/ContentTitle";
+import Link from "next/link";
 
 function Statistics() {
   return (
@@ -10,8 +10,10 @@ function Statistics() {
       <div className="w-[500px] h-[100px] bg-gray-100 border-t-2 border-b-2 border-gray-300 px-4 flex justify-between items-center">
         <p className="text-lg font-semibold">파이어베이스 통계</p>
         <Link
-          href=""
-          className="w-[100px] h-[40px] bg-blue-500 text-white text-lg flex justify-center items-center rounded-lg">
+          target="_blank"
+          href="https://console.firebase.google.com/project/k-luck/overview?pli=1"
+          className="w-[100px] h-[40px] bg-blue-500 text-white text-lg flex justify-center items-center rounded-lg"
+        >
           바로 가기
         </Link>
       </div>
