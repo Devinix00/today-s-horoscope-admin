97d4ab8003c7204b761cfe748e442e4cd9d75f60
Devinix00 <dpldpl123456@naver.com>
2024-06-11 12:32:08 +0900

    design: 대시보드 UI 수정

--- app/(withSidebar)/home/page.tsx
+++ app/(withSidebar)/home/page.tsx
@@ -4,17 +4,26 @@ import ShortcutsLink from "./_components/ShortcutsLink";
 import routes from "../../_constants/routes";
 import ShortcutsButton from "./_components/ShortcutsButton";
 import MainContainer from "../../_components/mainContainer/MainContainer";
+import dayjs from "dayjs";
 
 function Home() {
+  const today = dayjs().format("YYYY/MM/DD");
   return (
     <MainContainer>
       <div className="grid grid-cols-2 gap-14">
         <HomeContent title="콘텐츠 생성 현황">
-          <li>2024/08/15 생성 완료</li>
+          <div className="absolute top-0 h-12 flex items-center bg-blue-500 left-0 pl-4 w-full text-white">
+            {today}
+          </div>
+          <div className="absolute top-16">
+            <li>
+              <span className="text-blue-500">2024/08/15 생성 완료</span>
+            </li>
+          </div>
           <ShortcutsLink type={routes.CONTENTS_DB} href={routes.CONTENTS_DB} />
         </HomeContent>
         <HomeContent title="관리자 설정">
-          <li>관리자 관리 및 정보 수정</li>
+          <li>관리자 정보</li>
           <ShortcutsLink href={routes.ADMIN} />
         </HomeContent>
         <HomeContent title="통계">
@@ -24,17 +33,23 @@ function Home() {
         </HomeContent>
         <HomeContent title="기능 관리">
           <div className="flex flex-col justify-between">
-            <li className="pb-2 border-b-2 flex items-center justify-between">
-              <p>푸시메시지 발송시간 관리</p>
-              <ShortcutsButton type={routes.PUSH_MESSAGE} />
+            <li className="py-2 border-b-2 relative">
+              <span>푸시메시지 발송시간 관리</span>
+              <div className="absolute right-0 top-1/2 translate-y-[-50%]">
+                <ShortcutsButton type={routes.PUSH_MESSAGE} />
+              </div>
             </li>
-            <li className="py-2 border-b-2 flex items-center justify-between">
-              <p>콘텐츠 생성 관리</p>
-              <ShortcutsButton type={routes.CONTENTS_SETTING} />
+            <li className="py-2 border-b-2 relative">
+              <span>콘텐츠 생성 관리</span>
+              <div className="absolute right-0 top-1/2 translate-y-[-50%]">
+                <ShortcutsButton type={routes.CONTENTS_SETTING} />
+              </div>
             </li>
-            <li className="py-2 flex items-center justify-between">
-              <p>콘텐츠 프롬프트 관리 및 수정</p>
-              <ShortcutsButton type={routes.CONTENTS_DB} />
+            <li className="py-2 relative">
+              <span>콘텐츠 프롬프트 관리 및 수정</span>
+              <div className="absolute right-0 top-1/2 translate-y-[-50%]">
+                <ShortcutsButton type={routes.CONTENTS_DB} />
+              </div>
             </li>
           </div>
         </HomeContent>
