ca9d13bba32a45336d94128b9297afa26feb998b
Devinix00 <dpldpl123456@naver.com>
2024-06-14 15:17:51 +0900

    feat: 로그인 된 사용자들에게는 로그인 페이지 건너뛰기

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,13 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+import useLoggedInStore from "../_stores/useLoggedInStore";
 import LoginForm from "./_components/LoginForm";
+import { useRouter } from "next/navigation";
+import routes from "../_constants/routes";
 
 function Login() {
+  const { loggedIn } = useLoggedInStore();
+  const router = useRouter();
+  useEffect(() => {
+    if (loggedIn) router.push(routes.HOME);
+  }, [loggedIn, router]);
+
   return (
-    <div className="bg-[#f8f8f8] min-h-[calc(100vh-150px)] flex justify-center items-center">
-      <div className="w-[384px] flex flex-col justify-center items-center mb-24">
-        <h1 className="text-black text-[28px] font-semibold">관리자 로그인</h1>
-        <LoginForm />
-      </div>
-    </div>
+    <>
+      {loggedIn ? (
+        <div className="w-full min-h-[100vh] fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+          <p className="text-2xl">Loading...</p>
+        </div>
+      ) : (
+        <div className="bg-[#f8f8f8] min-h-[calc(100vh-150px)] flex justify-center items-center">
+          <div className="w-[384px] flex flex-col justify-center items-center mb-24">
+            <h1 className="text-black text-[28px] font-semibold">
+              관리자 로그인
+            </h1>
+            <LoginForm />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
